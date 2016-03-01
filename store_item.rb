b3a72ac2138441e5b664bd7761be34f1c3cda269
Carolyn Pescheret <crpescheret@gmail.com>
2016-02-29 21:19:04 -0600

    changed the way that the object is named

--- store_item.rb
+++ store_item.rb
@@ -5,15 +5,16 @@ product4 = {:name => "lock pick kit", :price => 235, :color => "red", :size => "
 
 puts "#{product1[:name]} is a #{product1[:size]} #{product1[:color]} product and is $#{product1[:price]}"
 puts "#{product2[:name]} is a #{product2[:size]} #{product1[:color]} product and is $#{product1[:price]}"
+puts "---------------"
 
 class Product
   attr_reader :name, :color
 
-  def initialize(input_name, input_price, input_color, input_size)
-    @name = input_name
-    @price = input_price
-    @size = input_size
-    @color = input_color
+  def initialize(input_options)
+    @name = input_options[:name]
+    @price = input_options[:price]
+    @size = input_options[:size]
+    @color = input_options[:color]
   end
 
   def print_info
@@ -25,10 +26,11 @@ class Product
   end
 end
 
-product1 = Product.new("magnifying glass", 25, "wood", "small")
-product2 = Product.new("trenchcoat", 130, "tan", "medium")
+product2 = Product.new(name: "night vision goggles", price: 130, color: "midnight", size: "medium")
+product1 = Product.new(:name => "magnifying glass", :price => 25, :color => "wood", :size => "small")
 product1.print_info
 product2.print_info
+puts "--------------"
 product1.wholesale_price
 product1.print_info
 puts product2.name
